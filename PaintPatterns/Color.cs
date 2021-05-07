02e7890c4d9422d9985c565457b3ed34d11cb0a0
redmarsprenger <redmarsprenger@gmail.com>
2021-05-07 16:33:56 +0200

    Stap 1

---
+++ PaintPatterns/Color.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace PaintPatterns
+{
+    public abstract class ColorDecorator
+    {
+        public Shape SelectedShape;
+    }
+
+    public class RedDecorator : ColorDecorator
+    {
+        private new Shape SelectedShape;
+
+        public RedDecorator(Shape selectedShape)
+        {
+            SelectedShape = selectedShape;
+            SelectedShape.Stroke = Brushes.Red;
+        }
+
+        public Shape ReturnShape()
+        {
+            return SelectedShape;
+        }
+    }
+}
---
+++ PaintPatterns/STAPPEN/Stap 1 eenvoudig tekenprogramma/Decorator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace PaintPatterns
+{
+    public abstract class ColorDecorator
+    {
+        public Shape SelectedShape;
+
+        public abstract Shape ReturnShape();
+    }
+
+    public class RedDecorator : ColorDecorator
+    {
+        public RedDecorator(Shape selectedShape)
+        {
+            SelectedShape = selectedShape;
+            SelectedShape.Fill = draw.randColor();
+        }
+
+        public override Shape ReturnShape()
+        {
+            return SelectedShape;
+        }
+    }
+}
